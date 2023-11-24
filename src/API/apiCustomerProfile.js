6100622e2e4d58994e4f83f621a269f61684be8a
Haizues1210 <104113556+Haizues1210@users.noreply.github.com>
2023-11-24 21:45:09 +0700

    Update apiCustomerProfile.js

--- src/API/apiCustomerProfile.js
+++ src/API/apiCustomerProfile.js
@@ -1,16 +1,1 @@
-import axiosClient from "./axiosClient.js";
 
-//link to api to get profile
-const GET_PROFILE_URL = "customers/profile";
-
-const apiCustomerProfile = {
-  getProfile(data) {
-    return axiosClient.get(GET_PROFILE_URL, {
-      headers: {
-        Authorization: `Bearer ${data.token}`,
-      },
-    });
-  },
-};
-
-export default apiCustomerProfile;
