07039f09bb94ae867338ce795daf0bc86667e50f
Thao Vy <88942210+covy2406@users.noreply.github.com>
2023-11-24 19:01:51 +0700

    Delete src/API directory

--- src/API/login.js
+++
@@ -1,9 +0,0 @@
-import axiosClient from "./axiosClient.js";
-
-const LOGIN_URL = "customers/login";
-
-function Login(data) {
-  return axiosClient.post(LOGIN_URL, data);
-}
-
-export default Login;
