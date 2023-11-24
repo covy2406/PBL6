8d04865ec748cac45a6799944db8ab178881943a
Haizues1210 <104113556+Haizues1210@users.noreply.github.com>
2023-11-24 21:45:26 +0700

    Update apiAuth.js

--- src/API/apiAuth.js
+++ src/API/apiAuth.js
@@ -1,16 +1,1 @@
-import axiosClient from "./axiosClient.js";
 
-//link api to login and signup
-const LOGIN_URL = "customers/login";
-const SIGNUP_URL = "customers/register";
-
-const apiAuth = {
-  signup(data) {
-    return axiosClient.post(SIGNUP_URL, data);
-  },
-  login(data) {
-    return axiosClient.post(LOGIN_URL, data);
-  },
-};
-
-export default apiAuth;
