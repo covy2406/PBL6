3a5d628a6bb592db6518f3c15f62e34330df7828
Haizues1210 <104113556+Haizues1210@users.noreply.github.com>
2023-12-06 19:28:30 +0700

    Update useAuth.js

--- src/Hook/useAuth.js
+++ src/Hook/useAuth.js
@@ -1,8 +1,1 @@
-import { useContext } from "react";
-import AuthContext from "../context/AuthProvider";
 
-const useAuth = () => {
-  const auth = useContext(AuthContext);
-  return auth;
-};
-export default useAuth;
