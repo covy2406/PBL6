90da0a2e8b3c6da26b6e4e6207673c5fd7ee6416
covy2406 <nguyentranthaovy2406@gmail.com>
2024-01-05 01:08:21 +0700

    shop products admin

--- src/Site_Admin/AdminProduct.jsx
+++ src/Site_Admin/AdminProduct.jsx
@@ -2,18 +2,17 @@ import "./css/AdminProductStyle.css";
 import useAdmin from "hook/useAdmin";
 import { useEffect, useState } from "react";
 // import { AiOutlineDown, AiOutlineShoppingCart, AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
-// import { BsEye } from "react-icons/bs";
 // import { AiOutlineStar } from "react-icons/ai";
-// import { Link, useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 // import useCartHandle from "hook/useCartHandle";
-// import useCart from "hook/useCart";
+import useCart from "hook/useCart";
 import useAuth from "hook/useAuth";
 
 function AdminProduct() {
   const { url } = useAuth();
   // const { addtocart } = useCartHandle();
-  // const { view } = useCart();
-  // const { id } = useParams();
+  const { view } = useCart();
+  const { id } = useParams();
   const { getAllShopProduct } = useAdmin();
   const [shopProductList, setShopProductList] = useState([]);
   useEffect(() => {
@@ -31,37 +30,39 @@ function AdminProduct() {
           <div className="admin__body-list">
             {Object.keys(shopProductList).map((itemProduct, index) => {
               return (
-                <li className="header__cart-item">
-                  <img
-                    src={`${url}${shopProductList[itemProduct].image}`}
-                    alt={shopProductList[itemProduct].name}
-                    className="header__cart-img"></img>
-                  <div className="header__cart-item-info">
-                    <div className="header__cart-item-head">
-                      <h5 className="header__cart-item-name">
-                        {shopProductList[itemProduct].name}
-                      </h5>
-                      <div className="header__cart-item-price-wrap">
-                        <span className="header__cart-item-price">
-                          {parseInt(
-                            shopProductList[itemProduct].price
-                          ).toLocaleString("vn-VN")}{" "}
-                          đ
-                        </span>
-                        <span className="header__cart-item-multiply"></span>
-                        <span className="header__cart-item-quantity">
-                          {/* {shopProductList[itemProduct].quantity_order} */}
-                        </span>
+                  <Link className="Admin__link-detail" to={`../../Viewdetail/${itemProduct.id}`}>
+                  <li className="header__cart-item icon__link"onClick={() => view(itemProduct.id)} key={index}>
+                      <img
+                        src={`${url}${shopProductList[itemProduct].image}`}
+                        alt={shopProductList[itemProduct].name}
+                        className="header__cart-img"></img>
+                      <div className="header__cart-item-info">
+                        <div className="header__cart-item-head">
+                          <h5 className="header__cart-item-name">
+                            {shopProductList[itemProduct].name}
+                          </h5>
+                          <div className="header__cart-item-price-wrap">
+                            <span className="header__cart-item-price">
+                              {parseInt(
+                                shopProductList[itemProduct].price
+                              ).toLocaleString("vn-VN")}{" "}
+                              đ
+                            </span>
+                            <span className="header__cart-item-multiply"></span>
+                            <span className="header__cart-item-quantity">
+                              {/* {shopProductList[itemProduct].quantity_order} */}
+                            </span>
+                          </div>
+                        </div>
+                        <div className="header__cart-item-body">
+                          <span className="header__cart-item-description">
+                            {shopProductList[itemProduct].description}
+                          </span>
+                          <div className="header__cart-item-remove"></div>
+                        </div>
                       </div>
-                    </div>
-                    <div className="header__cart-item-body">
-                      <span className="header__cart-item-description">
-                        {shopProductList[itemProduct].description}
-                      </span>
-                      <div className="header__cart-item-remove"></div>
-                    </div>
-                  </div>
-                </li>
+                  </li>
+                  </Link>
               );
             })}
           </div>
