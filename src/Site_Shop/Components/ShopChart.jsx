bb58a26de2ac7d3971b53a1b528170a03ce4af98
Haizues1210 <104113556+Lithesys@users.noreply.github.com>
2024-01-05 00:17:23 +0700

    Done chart, missing UI chart

--- src/Site_Shop/Components/ShopChart.jsx
+++ src/Site_Shop/Components/ShopChart.jsx
@@ -1,17 +1,7 @@
-import {
-  LineChart,
-  Line,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-  BarChart,
-  Bar,
-  Rectangle,
-} from "recharts";
 import { useState, useEffect } from "react";
 import apiChart from "api/apiChart";
+import PurchasePriceRangeStatistics from "./ShopChart/PurchasePriceRangeStatistics";
+import RevenueStatistics from "./ShopChart/RevenueStatistics";
 
 const ShopChart = () => {
   const [purchasePriceRangeStatistics, setPurchasePriceRangeStatistics] =
@@ -19,15 +9,8 @@ const ShopChart = () => {
   const [revenueStatistics, setRevenueStatistics] = useState([]);
 
   //data dữ liệu theo khoảng giá
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await apiChart.purchasePriceRangeStatistics();
-      //convert to array
-      setPurchasePriceRangeStatistics({ ...res.data, order_count: 2 });
-    };
-    fetchData();
-  }, []);
 
+  console.log(purchasePriceRangeStatistics);
   return (
     <>
       <div className="shop__container-nav">
@@ -38,62 +21,13 @@ const ShopChart = () => {
             </ul>
           </div>
         </div>
-        <div className="shop__chart">
-          <LineChart
-            width={730}
-            height={250}
-            data={purchasePriceRangeStatistics}
-            margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Line type="monotone" dataKey="pv" stroke="#8884d8" />
-            <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-          </LineChart>
-        </div>
-        <div className="shop__chart">
-          <BarChart
-            width={800}
-            height={300}
-            data={purchasePriceRangeStatistics.data}
-            margin={{
-              top: 10,
-              right: 30,
-              left: 20,
-              bottom: 5,
-            }}>
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis
-              dataKey="range"
-              tickFormatter={(value) => {
-                const [start, end] = value.split("-");
-                const startMillions = start / 1000000;
-                const endMillions = end / 1000000;
-                return `${startMillions}M - ${endMillions}M`;
-              }}
-            />
-            <YAxis dataKey="order_count" />
-            <Tooltip
-              content={({ payload }) => {
-                if (payload && payload.length > 0) {
-                  return (
-                    <div
-                      style={{
-                        backgroundColor: "#fff",
-                        padding: "0.5rem 1rem",
-                        border: "1px solid #ccc",
-                      }}>
-                      <p>Số lượng: {payload[0].value}</p>
-                    </div>
-                  );
-                }
-                return null;
-              }}
-            />
-            <Bar dataKey="order_count" fill="#8884d8" barSize={10} width={10} />
-          </BarChart>
+        <div className="shop__orders--list">
+          <div className="shop__chart">
+            <RevenueStatistics />
+          </div>
+          <div className="shop__chart">
+            <PurchasePriceRangeStatistics />
+          </div>
         </div>
       </div>
     </>
---
+++ src/Site_Shop/Components/ShopChart/PurchasePriceRangeStatistics.jsx
@@ -0,0 +1,110 @@
+import { useState, useEffect } from "react";
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip } from "recharts";
+//api
+import apiChart from "api/apiChart";
+
+const PurchasePriceRangeStatistics = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await apiChart.purchasePriceRangeStatistics();
+      setData({
+        ...res.data,
+        startDate: "2024-01-01",
+        endDate: "2024-01-31",
+      });
+    };
+    fetchData();
+  }, []);
+  console.log(data);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await apiChart.purchasePriceRangeStatistics(
+      data.startDate,
+      data.endDate
+    );
+    setData({
+      ...data,
+      data: res.data.data,
+    });
+  };
+
+  return (
+    <>
+      <BarChart
+        width={800}
+        height={300}
+        data={data.data}
+        margin={{
+          top: 10,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis
+          dataKey="range"
+          tickFormatter={(value) => {
+            const [start, end] = value.split("-");
+            const startMillions = start / 1000000;
+            const endMillions = end / 1000000;
+            return `${startMillions}M - ${endMillions}M`;
+          }}
+        />
+        <YAxis dataKey="order_count" />
+        <Tooltip
+          content={({ payload }) => {
+            if (payload && payload.length > 0) {
+              return (
+                <div
+                  style={{
+                    backgroundColor: "#fff",
+                    padding: "0.5rem 1rem",
+                    border: "1px solid #ccc",
+                  }}>
+                  <p>Số lượng: {payload[0].value}</p>
+                </div>
+              );
+            }
+            return null;
+          }}
+        />
+        <Bar dataKey="order_count" fill="#000000" barSize={10} width={10} />
+      </BarChart>
+      <div>Bắt đầu</div>
+      <input
+        className="productscard__container-update--body-item--input"
+        type="date"
+        name="startDate"
+        value={data.startDate}
+        onChange={(e) =>
+          setData({
+            ...data,
+            startDate: e.target.value,
+          })
+        }
+      />
+      <div>Kết thúc</div>
+      <input
+        className="productscard__container-update--body-item--input"
+        type="date"
+        name="startDate"
+        value={data.endDate}
+        onChange={(e) =>
+          setData({
+            ...data,
+            endDate: e.target.value,
+          })
+        }
+      />
+      <button
+        className="productscard__container-update--body-item--btn"
+        onClick={(e) => handleSubmit(e)}>
+        Xem
+      </button>
+    </>
+  );
+};
+
+export default PurchasePriceRangeStatistics;
